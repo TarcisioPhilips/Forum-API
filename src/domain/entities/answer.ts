f272f73b73a7ea01c325bc0473d478b30aac529a
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-03-04 14:34:27 -0300

    feat: adding some properties (updatedAt and createdAt)

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface AnswerProps{
-    content: string,
-    questionId : string, 
-    authorId: string
+    content: string
+    questionId : UniqueEntityID
+    authorId: UniqueEntityID
+    createdAt: Date
+    updatedAt?: Date
 }
 export class Answer extends Entity<AnswerProps> {
     get content() {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,14 +1,17 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
-    title:string, 
-    content: string, 
-    authorId: string,
+    title:string
+    content: string
+    authorId: UniqueEntityID
     slug: Slug
+    bestAnswerId ?: UniqueEntityID
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
-
 
 }
