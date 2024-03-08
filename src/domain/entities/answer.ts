c8bba637d3de7915f028f3d800c9fb4d2bbb9fa7
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-03-08 09:31:00 -0300

    feat: entity abstract creation

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,5 +1,6 @@
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/type/optional"
 
 interface AnswerProps{
     content: string
@@ -11,6 +12,15 @@ interface AnswerProps{
 export class Answer extends Entity<AnswerProps> {
     get content() {
         return this.props.content
+    }
+
+    static create(props:Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
+        const answer = new Answer({
+            ...props,
+            createdAt: new Date()
+        }, id)
+
+        return answer
     }
 
 }
