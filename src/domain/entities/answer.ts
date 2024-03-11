d40150ab4f33d853e15ab7956fbc0fd16ad681bd
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-03-11 11:25:47 -0300

    feat: getters and setters initial config

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,6 @@
-import { Entity } from "../../core/entities/entity"
-import { UniqueEntityID } from "../../core/entities/unique-entity-id"
-import { Optional } from "../../core/type/optional"
+import { Entity } from "@/core/entities/entity"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Optional } from "@/core/type/optional"
 
 interface AnswerProps{
     content: string
@@ -12,6 +12,38 @@ interface AnswerProps{
 export class Answer extends Entity<AnswerProps> {
     get content() {
         return this.props.content
+    }
+
+    get questionId() {
+        return this.props.questionId
+    }
+    
+    get authorId() {
+        return this.props.authorId
+    }
+        
+    get createdAt() {
+        return this.props.createdAt
+    }
+        
+    get updatedAt() {
+        return this.props.updatedAt
+    }
+
+    get excerpt() {
+        return this.content
+            .substring(0,120)
+            .trimEnd()
+            .concat('...')
+    }
+
+    private touch() {
+        this.props.updatedAt = new Date()
+    }
+
+    set content(content: string) {
+        this.props.content = content
+        this.touch()
     }
 
     static create(props:Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
