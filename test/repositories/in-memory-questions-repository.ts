2e92c95afe7b97f9290ad041bf8272131aabebf8
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-06-02 20:55:42 -0300

    faet: edit question use case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,37 +1,42 @@
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 
-export class InMemoryQuestionsRepository implements QuestionsRepository{
-    public items: Question[] = []
+export class InMemoryQuestionsRepository implements QuestionsRepository {
+  public items: Question[] = [];
 
-    async findById(id: string) {
-        const question = this.items.find((item) => item.id.toString() === id)
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id);
 
-        if (!question) {
-            return null
-        }
-
-        return question    
+    if (!question) {
+      return null;
     }
 
-    async findBySlug(slug: string){
-        const question = this.items.find((item) => item.slug.value ===slug)
+    return question;
+  }
 
-        if (!question) {
-            return null
-        }
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug);
 
-        return question
-    }
-    
-    async create(question: Question) {
-        this.items.push(question)
+    if (!question) {
+      return null;
     }
 
-    async delete(question: Question) {
-       const itemIndex = this.items.findIndex(item => item.id === question.id)
+    return question;
+  }
 
-       this.items.splice(itemIndex,1)
-    }
-    
+  async save(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+    this.items[itemIndex] = question;
+  }
+
+  async create(question: Question) {
+    this.items.push(question);
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+    this.items.splice(itemIndex, 1);
+  }
 }
