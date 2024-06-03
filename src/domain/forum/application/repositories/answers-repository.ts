c581e95740cc3d91555b74b6910fa573778043ae
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-06-02 21:02:13 -0300

    feat: edit answer use case

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,7 +1,8 @@
-import { Answer } from '../../enterprise/entities/answer'
+import { Answer } from "../../enterprise/entities/answer";
 
 export interface AnswersRepository {
-  findById(id: string): Promise<Answer | null>
-  create(answer: Answer): Promise<void>
-  delete(answer: Answer): Promise <void>
+  findById(id: string): Promise<Answer | null>;
+  save(answer: Answer): Promise<void>;
+  create(answer: Answer): Promise<void>;
+  delete(answer: Answer): Promise<void>;
 }
