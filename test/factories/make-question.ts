af49d51fd3bcb27ad0f6a7b2fa41d361de5a58f4
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-06-08 12:09:32 -0300

    fix: formating files

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,18 +1,23 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
-import {faker} from '@faker-js/faker'
+import {
+  Question,
+  QuestionProps,
+} from "@/domain/forum/enterprise/entities/question";
+import { faker } from "@faker-js/faker";
 
 export function makeQuestion(
   override: Partial<QuestionProps> = {},
   id?: UniqueEntityID
 ) {
-  const question = Question.create({
-    title: faker.lorem.sentence(),
-    authorId: new UniqueEntityID(),
-    content: faker.lorem.text(),
-    ...override
-  }, id)
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id
+  );
 
-  return question
+  return question;
 }
