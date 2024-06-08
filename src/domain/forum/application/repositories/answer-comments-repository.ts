aad016f1babf4ccac44de0bba20d99e991301dda
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-06-08 12:16:36 -0300

    feat: delete comment on answer use case

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,5 +1,7 @@
 import { AnswerComment } from "../../enterprise/entities/answer-comments";
 
 export interface AnswerCommentsRepository {
+  findById(ind: string): Promise<AnswerComment | null>;
+  delete(answerComment: AnswerComment): Promise<void>;
   create(answerComment: AnswerComment): Promise<void>;
 }
