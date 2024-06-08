0014ccb7bcfb5bab254515dd30bfe3e1660d2c35
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-06-08 16:36:33 -0300

    feat: fetch answer comments use case

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,7 +1,12 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerComment } from "../../enterprise/entities/answer-comments";
 
 export interface AnswerCommentsRepository {
   findById(ind: string): Promise<AnswerComment | null>;
+  findManyByAnswerId(
+    answerId: string,
+    params: PaginationParams
+  ): Promise<AnswerComment[]>;
   delete(answerComment: AnswerComment): Promise<void>;
   create(answerComment: AnswerComment): Promise<void>;
 }
